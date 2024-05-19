7e152584ccff93dc65d9e483630a8099bbe3bab5
Joe-Arisa <2861166295@qq.com>
2024-05-19 15:00:27 +0800

    add describe

--- src/main/java/com/example/MainExcel.java
+++ src/main/java/com/example/MainExcel.java
@@ -7,7 +7,8 @@ import com.example.listener.UserDataListener;
 public class MainExcel {
     public static void main(String[] args) {
 
-        String fileName = "/Users/lastaz3/Desktop/userExcel.xlsx";
+        //建议使用绝对地址
+        String fileName = "/Users/arisa/Desktop/userExcel.xlsx";
         // 这里默认读取第一个sheet
         EasyExcel.read(fileName, User.class, new UserDataListener()).sheet().doRead();
     }
